1e59588762dfc57d03ae5063f32568c561c2d469
gilajb <cjoybett@gmail.com>
2026-03-28 19:12:15 -0700

    modified:   backend/audit/__pycache__/urls.cpython-312.pyc 	modified:   backend/audit/admin.py 	modified:   backend/config/__pycache__/settings.cpython-312.pyc 	modified:   backend/config/settings.py     modified:   backend/contributions/__pycache__/urls.cpython-312.pyc     modified:   backend/contributions/admin.py 	modified:   backend/emergencies/__pycache__/urls.cpython-312.pyc 	modified:   backend/emergencies/admin.py 	modified:   backend/groups/__pycache__/urls.cpython-312.pyc 	modified:   backend/groups/admin.py 	deleted:    backend/mpesa/__init__.py     deleted:    backend/mpesa/__pycache__/__init__.cpython-312.pyc 	modified:   backend/mpesa/__pycache__/urls.cpython-312.pyc 	modified:   backend/mpesa/admin.py 	deleted:    backend/mpesa/apps.py 	deleted:    backend/mpesa/migrations/__init__.py 	deleted:    backend/mpesa/models.py 	deleted:    backend/mpesa/tests.py 	deleted:    backend/mpesa/views.py 	modified:   backend/notifications/__pycache__/urls.cpython-312.pyc 	modified:   backend/notifications/admin.py 	modified:   backend/users/admin.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,6 +51,7 @@ LOCAL_APPS = [
     'mpesa',
     'notifications',
     'audit',
+    'JamiiAfyaApp',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS # + LOCAL_APPS
--- backend/emergencies/admin.py
+++ backend/emergencies/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import EmergencyRequest, EmergencyDocument, EmergencyApproval
 
-# Register your models here.
+admin.site.register(EmergencyRequest)
+admin.site.register(EmergencyDocument)
+admin.site.register(EmergencyApproval)
